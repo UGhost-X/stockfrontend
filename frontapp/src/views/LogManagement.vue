8fa58018e080301173ebfe3729896d331b38f470
shixinqiu <1129202430@qq.com>
2024-03-26 23:20:53 +0800

    update logmanagement

--- frontapp/src/views/LogManagement.vue
+++ frontapp/src/views/LogManagement.vue
@@ -1,15 +1,13 @@
 <template>
   <a-row>
     <a-col :span="6">
-      <div style="background-color: black">1233</div>
+      <div class="functionalpannel">123</div>
     </a-col>
     <a-col :span="18" class="logstyle">
-      <div class="logbackground">
-        <div class="loginfo">
-          12222223 52333333 2223544 11132131 11313dddddddddddddddddddddddddddd
-          11313dddddddddddddddddddddddddddd 11313dddddddddddddddddddddddddddd
-          11313dddddddddddddddddddddddddddd
-        </div>
+      <div class="loginfo">
+        12222223 52333333 2223544 11132131 11313dddddddddddddddddddddddddddd
+        11313dddddddddddddddddddddddddddd 11313dddddddddddddddddddddddddddd
+        11313dddddddddddddddddddddddddddd
       </div>
     </a-col>
   </a-row>
@@ -21,36 +19,41 @@ import { ref } from "vue";
 
 <style scoped>
 .logstyle {
-  background-image: url("@/assets/pictures/logtelevison3.png");
+  background-image: url("@/assets/pictures/logtelevison.png");
   background-repeat: no-repeat;
   background-position: right;
   background-size: 100% 100%;
   height: calc(100vh - 83px);
   width: 100%;
-  /* margin: 50px 50px 0 0; */
-  /* padding: 50px 50px 0 0; */
   border-width: 50px 50px 0 0;
   border-style: solid;
   border-color: transparent;
   position: relative;
 }
-.logbackground{
-  background: linear-gradient(31deg,#65799ba3,#5e2563);
-  z-index:-999;
-}
+
 .loginfo {
   position: absolute;
   font-size: 102px;
-  /* height: 100%; */
+  /* background-color: rgba(0, 0, 0, 0.336); */
   left: calc(100vw - 94vw);
-  top: 90px;
-  bottom: 200px;
+  top: calc(10vh);
+  bottom: calc(50vh);
   padding: 0 0 0 0;
-  max-height: calc(100vh);
-  max-width: calc(100vw - 53vw);
+  /* height: 100px; */
+  height: calc(50vh);
+  width: calc(47vw);
   overflow-y: scroll;
   overflow-wrap: break-word;
 }
+.functionalpannel {
+  height: 100%;
+  width: 100%;
+  background-color: aquamarine;
+  border-radius: 20px;
+  margin: 10px 10px 10px 0;
+  box-shadow: 5px 5px 5px 5px rgba(53, 49, 49, 0.137);
+}
+
 ::-webkit-scrollbar-thumb {
   background-color: transparent;
 }
